c60fbc21c0e6a2853d0af26ffa34d9f4e748cc36
Luheni <pluheni@gmail.com>
2024-05-13 07:13:14 +0200

    program

---
+++ progam.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int brUcenika;
+    cin >> brUcenika;
+    cin.ignore();
+
+    string ime_prezime[100];
+    double prosjek[100];
+
+    for (int i = 0; i < brUcenika; i++)
+    {
+        cin.ignore();
+        getline(cin, ime_prezime[i]);
+        cin >> prosjek[i];
+    }
+
+    for (int i = 0; i < brUcenika - 1; i++)
+    {
+        for (int j = i + 1; j < brUcenika; j++)
+        {
+            if (prosjek[i] < prosjek[j])
+            {
+                swap(prosjek[i], prosjek[j]);
+                swap(ime_prezime[i], ime_prezime[j]);
+            }
+        }
+    }
+
+    fstream datotekaOut("imenik.txt", ios::binary | ios::out);
+    for (int i = 0; i < brUcenika; i++)
+    {
+        datotekaOut.write((char *)&prosjek[i], sizeof(double));
+        int len = ime_prezime[i].size();
+        datotekaOut.write((char *)&len, sizeof(int));
+        datotekaOut.write((char *)&ime_prezime[i], sizeof(string));
+    }
+    datotekaOut.close();
+
+    fstream datotekaIn("imenik.txt", ios::binary | ios::in);
+    for (int i = 0; i < brUcenika; i++)
+    {
+        string ime_prezime;
+        double prosjek;
+        datotekaIn.read((char *)&prosjek, sizeof(double));
+        int len;
+        datotekaIn.read((char *)&len, sizeof(int));
+        char *buffer = new char[len + 1];
+        datotekaIn.read(buffer, len);
+        buffer[len] = '\0';
+        ime_prezime = buffer;
+        delete[] buffer;
+        cout << ime_prezime << " " << prosjek << endl;
+    }
+    datotekaIn.close();
+    return 0;
+}
